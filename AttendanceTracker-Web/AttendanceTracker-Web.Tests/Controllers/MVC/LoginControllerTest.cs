6f74a54fca4a836c24235990dea4f951fb65f56d
Greyson Wright <greysonmwright@gmail.com>
2018-10-17 15:34:12 -0500

    - configured unit test classes

---
+++ AttendanceTracker-Web/AttendanceTracker-Web.Tests/Controllers/MVC/LoginControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AttendanceTracker_Web;
+using AttendanceTracker_Web.Controllers.MVC;
+
+namespace AttendanceTracker_Web.Tests.Controllers.MVC
+{
+    [TestClass]
+    public class LoginControllerTest
+    {
+        [TestMethod]
+        public void Login()
+        {
+            // Arrange
+            LoginController controller = new LoginController();
+
+            // Act
+            ViewResult result = controller.Login() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- AttendanceTracker-Web/AttendanceTracker-Web/Controllers/MVC/HomeController.cs
+++ AttendanceTracker-Web/AttendanceTracker-Web/Controllers/MVC/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace AttendanceTracker_Web.Controllers
+namespace AttendanceTracker_Web.Controllers.MVC
 {
     public class HomeController : Controller
     {
