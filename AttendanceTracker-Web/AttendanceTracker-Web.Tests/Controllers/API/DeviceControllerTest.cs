6f2e5a5fae2a0542fcb99ce741a1ce13cbbd5fb0
Greyson Wright <greysonmwright@gmail.com>
2018-11-02 20:06:37 -0500

    - added devicecontroller - added unittest for devicecontroller

---
+++ AttendanceTracker-Web/AttendanceTracker-Web.Tests/Controllers/API/DeviceControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AttendanceTracker_Web.Tests.Controllers.API
+{
+    [TestClass]
+    public class DeviceControllerTest
+    {
+        [TestMethod]
+        public void VerifyDevice()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void RegisterDevice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
