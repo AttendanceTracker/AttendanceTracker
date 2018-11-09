e01786c9966ec02ae6cbdabc8bf497b42f4dca8c
Greyson Wright <greysonmwright@gmail.com>
2018-11-08 20:26:03 -0600

    - fixed routing issue with duplicate web method

--- AttendanceTracker-Web/AttendanceTracker-Web/App_Start/WebApiConfig.cs
+++ AttendanceTracker-Web/AttendanceTracker-Web/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace AttendanceTracker_Web
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
