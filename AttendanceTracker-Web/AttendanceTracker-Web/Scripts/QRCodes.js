c2e723df704bc819a89ebbf01e61598a64f66674
Greyson Wright <greysonmwright@gmail.com>
2018-12-04 04:27:17 -0600

    - fixed displayed dates in qrcodes

--- AttendanceTracker-Web/AttendanceTracker-Web/Scripts/QRCodes.js
+++ AttendanceTracker-Web/AttendanceTracker-Web/Scripts/QRCodes.js
@@ -1,81 +1,91 @@
-﻿angular.module("QRCodesModule", [])
-    .controller("QRCodesController", function ($scope, $http) {
-        var qrCodesController = this;
+﻿var qrCodesModule = angular.module("QRCodesModule", []);
 
-        qrCodesController.qrCodeData = [];
-        qrCodesController.classData = [];
-        qrCodesController.dialog = {};
+qrCodesModule.filter('dayOfWeek', function () {
+    return function (date) {
+        var actualDate = new Date(date);
+        var day = actualDate.getDay();
+        var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        return weekday[day];
+    }
+});
 
-        qrCodesController.$onInit = function () {
-            qrCodesController.getQRCodes();
-            qrCodesController.getClasses();
-        };
+qrCodesModule.controller("QRCodesController", function ($scope, $http) {
+    var qrCodesController = this;
 
-        qrCodesController.getQRCodes = function () {
-            $http.get("/Home/GetActiveQRCodes").then(
-                function (response) {
-                    qrCodesController.qrCodeData = response.data;
-                },
-                function (error) {
-                    console.log(error);
-                }
-            );
-        };
+    qrCodesController.qrCodeData = [];
+    qrCodesController.classData = [];
+    qrCodesController.dialog = {};
 
-        qrCodesController.getClasses = function () {
-            $http.get("/Home/GetClasses").then(
-                function (response) {
-                    qrCodesController.classData = response.data;
-                },
-                function (error) {
-                    console.log(error);
-                }
-            );
-        };
+    qrCodesController.$onInit = function () {
+        qrCodesController.getQRCodes();
+        qrCodesController.getClasses();
+    };
 
-        qrCodesController.addModalButtonClicked = function () {
-            var classSelectText = $("#class-select").val();
-            if (classSelectText == "") {
-                return qrCodesController.showToast("Please select a class.");
+    qrCodesController.getQRCodes = function () {
+        $http.get("/Home/GetActiveQRCodes").then(
+            function (response) {
+                qrCodesController.qrCodeData = response.data;
+            },
+            function (error) {
+                console.log(error);
             }
+        );
+    };
 
-            var classID = qrCodesController.classData.find(function (element) {
-                return element.Name == classSelectText;
-            }).ID;
+    qrCodesController.getClasses = function () {
+        $http.get("/Home/GetClasses").then(
+            function (response) {
+                qrCodesController.classData = response.data;
+            },
+            function (error) {
+                console.log(error);
+            }
+        );
+    };
 
-            var config = { params: { classID: classID, expiresIn: 10000 } };
-            $http.post("/Home/AddQRCode", null, config).then(
-                function (response) {
-                    if (response.status = 200) {
-                        qrCodesController.getQRCodes();
-                        qrCodesController.dialog.close();
-                        qrCodesController.showToast("QR code created.");
-                    } else {
-                        qrCodesController.showToast("Failed to create QR code.");
-                    }
-                },
-                function (error) {
-                    console.log(error);
+    qrCodesController.addModalButtonClicked = function () {
+        var classSelectText = $("#class-select").val();
+        if (classSelectText == "") {
+            return qrCodesController.showToast("Please select a class.");
+        }
+
+        var classID = qrCodesController.classData.find(function (element) {
+            return element.Name == classSelectText;
+        }).ID;
+
+        var config = { params: { classID: classID, expiresIn: 10000 } };
+        $http.post("/Home/AddQRCode", null, config).then(
+            function (response) {
+                if (response.status = 200) {
+                    qrCodesController.getQRCodes();
+                    qrCodesController.dialog.close();
+                    qrCodesController.showToast("QR code created.");
+                } else {
                     qrCodesController.showToast("Failed to create QR code.");
                 }
-            );
-        };
-
-        qrCodesController.addQRCodeButtonClicked = function () {
-            qrCodesController.dialog = document.querySelector('#qr-modal');
-            angular.element(qrCodesController.dialog).ready(function () {
-                componentHandler.upgradeAllRegistered();
-            });
-            if (!qrCodesController.dialog.showModal) {
-                dialogPolyfill.registerDialog(qrCodesController.dialog);
+            },
+            function (error) {
+                console.log(error);
+                qrCodesController.showToast("Failed to create QR code.");
             }
-            qrCodesController.dialog.showModal();
-        };
+        );
+    };
 
-        qrCodesController.showToast = function (message) {
-            var snackbarContainer = document.querySelector('#toast-container');
-            'use strict';
-            var data = { message: message };
-            snackbarContainer.MaterialSnackbar.showSnackbar(data);
-        };
-    });
+    qrCodesController.addQRCodeButtonClicked = function () {
+        qrCodesController.dialog = document.querySelector('#qr-modal');
+        angular.element(qrCodesController.dialog).ready(function () {
+            componentHandler.upgradeAllRegistered();
+        });
+        if (!qrCodesController.dialog.showModal) {
+            dialogPolyfill.registerDialog(qrCodesController.dialog);
+        }
+        qrCodesController.dialog.showModal();
+    };
+
+    qrCodesController.showToast = function (message) {
+        var snackbarContainer = document.querySelector('#toast-container');
+        'use strict';
+        var data = { message: message };
+        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+    };
+});
