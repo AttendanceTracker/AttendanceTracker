4b78b8cf72f869defd4bb84d9b901893e3fb0c9f
Greyson Wright <greysonmwright@gmail.com>
2018-12-04 19:05:01 -0600

    - implemented angularjs material modal in qrcodes - added time-picker to add qr modal

--- AttendanceTracker-Web/AttendanceTracker-Web/Scripts/QRCodes.js
+++ AttendanceTracker-Web/AttendanceTracker-Web/Scripts/QRCodes.js
@@ -1,4 +1,4 @@
-﻿var qrCodesModule = angular.module("QRCodesModule", []);
+﻿var qrCodesModule = angular.module("QRCodesModule", ["ngAnimate", "ngMaterial", "md.time.picker"]);
 
 qrCodesModule.filter('dayOfWeek', function () {
     return function (date) {
@@ -9,16 +9,14 @@ qrCodesModule.filter('dayOfWeek', function () {
     }
 });
 
-qrCodesModule.controller("QRCodesController", function ($scope, $http) {
+qrCodesModule.controller("QRCodesController", ["$scope", "$http", "$mdDialog", "$mdpTimePicker", function ($scope, $http, $mdDialog) {
     var qrCodesController = this;
 
     qrCodesController.qrCodeData = [];
-    qrCodesController.classData = [];
     qrCodesController.dialog = {};
 
     qrCodesController.$onInit = function () {
         qrCodesController.getQRCodes();
-        qrCodesController.getClasses();
     };
 
     qrCodesController.getQRCodes = function () {
@@ -32,44 +30,33 @@ qrCodesModule.controller("QRCodesController", function ($scope, $http) {
         );
     };
 
-    qrCodesController.getClasses = function () {
-        $http.get("/Home/GetClasses").then(
-            function (response) {
-                qrCodesController.classData = response.data;
-            },
-            function (error) {
-                console.log(error);
-            }
-        );
-    };
+    //qrCodesController.addModalButtonClicked = function () {
+    //    var classSelectText = $("#class-select").val();
+    //    if (classSelectText == "") {
+    //        return qrCodesController.showToast("Please select a class.");
+    //    }
 
-    qrCodesController.addModalButtonClicked = function () {
-        var classSelectText = $("#class-select").val();
-        if (classSelectText == "") {
-            return qrCodesController.showToast("Please select a class.");
-        }
+    //    //var classID = qrCodesController.classData.find(function (element) {
+    //    //    return element.Name == classSelectText;
+    //    //}).ID;
 
-        var classID = qrCodesController.classData.find(function (element) {
-            return element.Name == classSelectText;
-        }).ID;
-
-        var config = { params: { classID: classID, expiresIn: 10000 } };
-        $http.post("/Home/AddQRCode", null, config).then(
-            function (response) {
-                if (response.status = 200) {
-                    qrCodesController.getQRCodes();
-                    qrCodesController.dialog.close();
-                    qrCodesController.showToast("QR code created.");
-                } else {
-                    qrCodesController.showToast("Failed to create QR code.");
-                }
-            },
-            function (error) {
-                console.log(error);
-                qrCodesController.showToast("Failed to create QR code.");
-            }
-        );
-    };
+    //    var config = { params: { classID: $scope.class.ID, expiresIn: 10000 } };
+    //    $http.post("/Home/AddQRCode", null, config).then(
+    //        function (response) {
+    //            if (response.status = 200) {
+    //                qrCodesController.getQRCodes();
+    //                qrCodesController.dialog.close();
+    //                qrCodesController.showToast("QR code created.");
+    //            } else {
+    //                qrCodesController.showToast("Failed to create QR code.");
+    //            }
+    //        },
+    //        function (error) {
+    //            console.log(error);
+    //            qrCodesController.showToast("Failed to create QR code.");
+    //        }
+    //    );
+    //};
 
     qrCodesController.addQRCodeButtonClicked = function () {
         qrCodesController.dialog = document.querySelector('#qr-modal');
@@ -88,4 +75,64 @@ qrCodesModule.controller("QRCodesController", function ($scope, $http) {
         var data = { message: message };
         snackbarContainer.MaterialSnackbar.showSnackbar(data);
     };
-});
+
+    $scope.showAdvanced = function (ev) {
+        $mdDialog.show({
+            controller: dialogController,
+            templateUrl: '/Views/Templates/QRCodeModal.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true
+        })
+    };
+
+    function dialogController($scope, $http, $mdDialog) {
+        $scope.classData = [];
+        $scope.class = {};
+        $scope.startTime = new Date();
+        $scope.endTime = new Date();
+        $scope.timePickerError = {
+            hour: 'Hour is required',
+            minute: 'Minute is required'
+        }
+
+        $scope.getClasses = function () {
+            $http.get("/Home/GetClasses").then(
+                function (response) {
+                    $scope.classData = response.data;
+                },
+                function (error) {
+                    console.log(error);
+                }
+            );
+        };
+
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+
+        $scope.add = function () {
+            if ($scope.class == {}) {
+                qrCodesController.showToast("Please select a class.");
+                return
+            }
+
+            var config = { params: { classID: $scope.class.ID, expiresIn: 10000 } };
+            $http.post("/Home/AddQRCode", null, config).then(
+                function (response) {
+                    if (response.status = 200) {
+                        qrCodesController.getQRCodes();
+                        qrCodesController.showToast("QR code created.");
+                        $mdDialog.cancel();
+                    } else {
+                        qrCodesController.showToast("Failed to create QR code.");
+                    }
+                },
+                function (error) {
+                    console.log(error);
+                    qrCodesController.showToast("Failed to create QR code.");
+                }
+            );
+        };
+    }
+}]);
